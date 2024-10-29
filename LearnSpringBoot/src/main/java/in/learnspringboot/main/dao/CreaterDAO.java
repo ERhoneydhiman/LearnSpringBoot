9af31d9be0a5c4359fe1bbef07a22cfa3e647fc1
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-10-29 15:13:26 +0530

    update creater profile API created with exception handling

--- LearnSpringBoot/src/main/java/in/learnspringboot/main/dao/CreaterDAO.java
+++ LearnSpringBoot/src/main/java/in/learnspringboot/main/dao/CreaterDAO.java
@@ -16,45 +16,63 @@ import in.learnspringboot.main.translaters.CreaterTranslater;
 
 @Repository
 public class CreaterDAO {
-	
+
 	@Autowired
 	private CreaterRepo createrRepo;
-	
+
 	@Autowired
 	private CreaterTranslater createrTranslater;
-	
-	public CreaterResDTO saveCreater(CreaterReqDTO createrReqDTO) throws UniqueNameAllreadyExist{
-		
+
+//	<----------------------------------------------------------------------------------------------------------------->
+
+	public CreaterResDTO saveCreater(CreaterReqDTO createrReqDTO) throws UniqueNameAllreadyExist {
+
 		Creater creater = createrTranslater.createrDtoToEntity(createrReqDTO);
 		if (createrRepo.existsByuniqueNameString(creater.getUniqueNameString())) {
 			throw new UniqueNameAllreadyExist("creater unique name already exist...");
 		}
-		Creater savedCreater=createrRepo.save(creater);
+		Creater savedCreater = createrRepo.save(creater);
 		CreaterResDTO resUser = createrTranslater.createrEntityToDto(savedCreater);
-		
+
 		return resUser;
-		
+
 	}
-	
-	
+
+//	<----------------------------------------------------------------------------------------------------------------->
+
 	public CreaterResDTO loginCreater(String uniqueName, String password) throws Exception {
 
-	    if (!createrRepo.existsByuniqueNameString(uniqueName)) {
-	        throw new UniqueNameNotFoundException("Creater not found...");
-	    }
+		if (!createrRepo.existsByuniqueNameString(uniqueName)) {
+			throw new UniqueNameNotFoundException("Creater not found...");
+		}
 
-	   
-	    Optional<Creater> createrOptional = createrRepo.getByuniqueNameString(uniqueName);
-	    Creater creater = createrOptional.orElseThrow(() -> new UniqueNameNotFoundException("Creater not found..."));
+		Optional<Creater> createrOptional = createrRepo.getByuniqueNameString(uniqueName);
+		Creater creater = createrOptional.orElseThrow(() -> new UniqueNameNotFoundException("Creater not found..."));
 
-	    if (!creater.getPasswordString().equals(password)) {
-	        throw new BadCredentialsException("Incorrect password.");
-	    }
+		if (!creater.getPasswordString().equals(password)) {
+			throw new BadCredentialsException("Incorrect password.");
+		}
 
-	    CreaterResDTO createrResDTO = createrTranslater.createrEntityToDto(creater);
+		CreaterResDTO createrResDTO = createrTranslater.createrEntityToDto(creater);
 
-	    return createrResDTO;
+		return createrResDTO;
 	}
 
+//	<----------------------------------------------------------------------------------------------------------------->
+
+	public Optional<Creater> findCreaterByUniqueName(String uniqueNameString) throws UniqueNameNotFoundException {
+		
+		return createrRepo.getByuniqueNameString(uniqueNameString);
+		
+	}
+//	<----------------------------------------------------------------------------------------------------------------->
+	
+	public Creater updateCreater(Creater creater) {
+		
+		return createrRepo.save(creater);
+		
+	}
+	
+	
 
 }
--- LearnSpringBoot/src/main/java/in/learnspringboot/main/services/CreaterService.java
+++ LearnSpringBoot/src/main/java/in/learnspringboot/main/services/CreaterService.java
@@ -3,16 +3,22 @@ package in.learnspringboot.main.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import in.learnspringboot.main.Entity.Creater;
 import in.learnspringboot.main.dao.CreaterDAO;
 import in.learnspringboot.main.dto.CreaterReqDTO;
 import in.learnspringboot.main.dto.CreaterResDTO;
 import in.learnspringboot.main.exceptions.UniqueNameAllreadyExist;
+import in.learnspringboot.main.exceptions.UniqueNameNotFoundException;
+import in.learnspringboot.main.translaters.CreaterTranslater;
 
 @Service
 public class CreaterService implements CreateServiceInterface{
 	
 	@Autowired
 	private CreaterDAO createrDAO;
+	
+	@Autowired
+	private CreaterTranslater createrTranslater;
 
 	@Override
 	public CreaterResDTO signInCreater(CreaterReqDTO createrReqDTO) throws UniqueNameAllreadyExist{
@@ -27,6 +33,29 @@ public class CreaterService implements CreateServiceInterface{
 		
 		CreaterResDTO createrResDTO = createrDAO.loginCreater(uniqueName, password);
 		return createrResDTO;
+		
+	}
+	
+	public CreaterResDTO getCreaterByUniqueName(String uniqueNameString) throws Exception{
+		
+		Creater creater = createrDAO.findCreaterByUniqueName(uniqueNameString).orElseThrow(()->new UniqueNameNotFoundException("creater not found..."));
+		CreaterResDTO createrResDTO = createrTranslater.createrEntityToDto(creater);
+		return createrResDTO;
+		
+	}
+	
+	public CreaterResDTO updateCreater(String uniqueNameString, CreaterReqDTO createrReqDTO) throws Exception{
+		
+		Creater creater = createrDAO.findCreaterByUniqueName(uniqueNameString).orElseThrow(()->new UniqueNameNotFoundException("creater not found..."));
+		
+		creater.setEmailString(createrReqDTO.getEmailString());
+		creater.setNameString(createrReqDTO.getNameString());
+		creater.setPhoneNoLong(createrReqDTO.getPhoneNoLong());
+		creater.setNicheOfCreaterString(createrReqDTO.getNicheOfCreaterString());
+		
+		Creater updatedCreater = createrDAO.updateCreater(creater);
+		
+		return createrTranslater.createrEntityToDto(updatedCreater);
 		
 	}
 
